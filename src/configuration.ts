e9df67d8a2276ca18485523b73b6381339983f8b
Filip Paulů <filip.paulu@datamole.cz>
2019-04-01 14:23:05 +0200

    New default configuration

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,24 +1,25 @@
 import { Application } from "./types";
 
-import { alertFromNumber } from "./alerts";
+import { alertFromNumber, togglStatus, alertChat, alertTestTitle } from "./alerts";
 
 const windows: Array<Application> = [
     {
         title: "Queeg",
         icon: "icon.png",
         items: [
-            { name: "gmail_work", icon: "gmail.png", color: "#8aca3a", url: "https://mail.google.com/mail/u/0/#inbox", partition: "persist:work", alert: alertFromNumber },
-            { name: "calendar_work", icon: "calendar.png", color: "#8aca3a", url: "https://calendar.google.com/calendar/b/0/", partition: "persist:work" },
-            { name: "chat_work", icon: "chat.png", color: "#8aca3a", url: "https://chat.google.com/u/0/", partition: "persist:work" },
-            { name: "toggl_work", icon: "toggl.png", color: "#8aca3a", url: "https://toggl.com/app/timer", partition: "persist:work" },
-            { name: "trello_work", icon: "trello.png", color: "#8aca3a", url: "https://trello.com/", partition: "persist:work" }, 
+            { name: "Gmail work", icon: "gmail.png", color: "#8aca3a", url: "https://mail.google.com/mail/u/0/#inbox", partition: "persist:work", alert: alertFromNumber },
+            { name: "Calendar work", icon: "calendar.png", color: "#8aca3a", url: "https://calendar.google.com/calendar/b/0/", partition: "persist:work", alert: alertTestTitle },
+            { name: "Chat work", icon: "chat.png", color: "#8aca3a", url: "https://chat.google.com/u/0/", partition: "persist:work", alert: alertChat },
+            { name: "Drive work", icon: "drive.png", color: "#8aca3a", url: "https://drive.google.com/drive/u/0/my-drive", partition: "persist:work" },
+            { name: "Toggl work", icon: "toggl.png", color: "#8aca3a", url: "https://toggl.com/app/timer", partition: "persist:work", alert: togglStatus },
+            { name: "Trello work", icon: "trello.png", color: "#8aca3a", url: "https://trello.com/", partition: "persist:work", alert: alertTestTitle }, 
             
-            { name: "gmail_personal", icon: "gmail.png", color: "#4286f4", url: "https://mail.google.com/mail/u/0/#inbox", partition: "persist:personal", alert: alertFromNumber },
-            { name: "spotify_personal", icon: "spotify.png", color: "#4286f4", url: "https://open.spotify.com/browse/featured", partition: "persist:personal" },
-            { name: "drive_personal", icon: "drive.png", color: "#4286f4", url: "https://drive.google.com/drive/u/0/my-drive", partition: "persist:personal" },
-            { name: "keep_personal", icon: "keep.png", color: "#4286f4", url: "https://keep.google.com/u/0/", partition: "persist:personal" },
-            { name: "messages_personal", icon: "messages.png", color: "#4286f4", url: "https://messages.android.com/", partition: "persist:personal" },
-            { name: "mess_personal", icon: "messenger.png", color: "#4286f4", url: "https://www.messenger.com/", partition: "persist:personal" },
+            { name: "Gmail personal", icon: "gmail.png", color: "#4286f4", url: "https://mail.google.com/mail/u/0/#inbox", partition: "persist:personal", alert: alertFromNumber },
+            // { name: "Spotify personal", icon: "spotify.png", color: "#4286f4", url: "https://open.spotify.com/browse/featured", partition: "persist:personal" },
+            { name: "Drive personal", icon: "drive.png", color: "#4286f4", url: "https://drive.google.com/drive/u/0/my-drive", partition: "persist:personal" },
+            { name: "Keep personal", icon: "keep.png", color: "#4286f4", url: "https://keep.google.com/u/0/", partition: "persist:personal" },
+            { name: "Messages personal", icon: "messages.png", color: "#4286f4", url: "https://messages.google.com/web/conversations", partition: "persist:personal", alert: alertFromNumber },
+            { name: "Messenger personal", icon: "messenger.png", color: "#4286f4", url: "https://www.messenger.com/", partition: "persist:personal", alert: alertFromNumber },
         ]
     }
 ];
