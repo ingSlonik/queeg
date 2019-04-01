58674e44e556cd900862e25717ddd972a43dc230
Filip Paulů <filip.paulu@datamole.cz>
2019-04-01 14:22:24 +0200

    Revrite renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,50 +2,70 @@ import { shell } from "electron";
 
 import configuration from "./configuration";
 
+function getElement(tag: string, properties: { [key: string]: any }, children?: Array<HTMLElement>): HTMLElement {
+    const element = document.createElement(tag);
+    Object.keys(properties).forEach(prop => element[prop] = properties[prop]);
+    if (children) {
+        children.forEach(child => element.appendChild(child));
+    }
+    return element;
+}
+
 export default function renderer(applicationIndex: number) {
     const application = configuration[applicationIndex];
-    
+
     const body = document.body;
     const nav = document.getElementById("nav");
 
     document.title = application.title;
-    
+
     const buttons = [];
     const webviews = [];
 
-    application.items.forEach(({ name, icon, url, partition, alert, color }, i) => {
+    application.items.forEach((item, i) => {
+        const { name, icon, url, partition, alert, color } = item;
 
         // button
-        const button = document.createElement("div");
-        button.className = i === 0 ? "button active" : "button";
-        button.onclick = () => {
-            buttons.forEach(b => b.className = "button");
-            button.className = "button active";
-            webviews.forEach(w => w.style.display = "none");
-            webview.style.display = "flex";
-        };
+        const background = getElement("div", { className: "background" }, []);
+        const number = getElement("div", { className: "number" }, []);
+        const partitionElement = getElement("div", { className: "partition", innerText: partition.replace("persist:", "").charAt(0).toLocaleUpperCase() });
+        const img = getElement("img", { alt: name, src: `../img/${icon}` });
+        const titleElement = getElement("div", { className: "title" });
+        const label = getElement("div", { className: "label" }, [ 
+            getElement("div", { className: "text" }, [
+                getElement("div", { className: "name", innerText: name }),
+                titleElement,
+            ]),
+            getElement("div", { className: "arrow" }),
+        ]);
 
-        nav.appendChild(button);
-        buttons.push(button);
-        const number = document.createElement("div");
-        number.className = "number";
-        button.appendChild(number);
-        const partitionElement = document.createElement("div");
-        partitionElement.className = "partition";
-        partitionElement.innerText = partition.replace("persist:", "").charAt(0).toLocaleUpperCase();
-        button.appendChild(partitionElement);
-        const background = document.createElement("div");
-        background.className = "background";
-        button.appendChild(background);
-        const img = document.createElement("img");
-        img.alt = name;
-        img.src = `../img/${icon}`;
-        button.appendChild(img);
+        const button = getElement(
+            "div",
+            {
+                className: i === 0 ? "button active" : "button",
+                onclick: () => {
+                    buttons.forEach(b => b.className = "button");
+                    button.className = "button active";
+                    webviews.forEach(w => w.style.display = "none");
+                    webview.style.display = "flex";
+                }
+            },
+            [
+                background,
+                number,
+                partitionElement,
+                img,
+                label,
+            ]
+        );
 
         if (color) {
             button.style.borderLeftColor = color;
             background.style.backgroundColor = color;
         }
+
+        nav.appendChild(button);
+        buttons.push(button);
 
         // webview
         const webview = document.createElement("webview");
@@ -55,8 +75,12 @@ export default function renderer(applicationIndex: number) {
         webview.addEventListener("new-window", (e) => {
             shell.openExternal(e.url)
         });
+        webview.addEventListener("page-title-updated", (e) => {
+            titleElement.innerText = e.title;
+        });
+
         if (alert) {
-            alert(webview, (alert) => {
+            alert(item, webview, (alert) => {
                 if (alert) {
                     number.innerText = alert;
                     number.style.display = "block";
