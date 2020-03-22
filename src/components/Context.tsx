8ee35123e38e7d6761cefeddc97f09b5a76ee9a4
Filip Paulů <ing.fenix@seznam.cz>
2020-03-22 21:21:49 +0100

    Update context to set window properties

--- src/components/Context.tsx
+++ src/components/Context.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
 
-import { getItems, addItem, setItem, deleteItem } from "../services/db";
+import { addWindow, setWindow, deleteWindow, getItems, addItem, setItem, deleteItem, getWindows } from "../services/db";
 
 import { Item, StaticItemProps, DynamicItemProps } from "../types";
 
-type ContextValue = {
+type WindowProps = {
     title: string,
     icon: string,
-    nav: { position: "left" | "top" },
+    nav: { position: "left" | "top", size: number },
+};
+
+type ContextValue = WindowProps & {
+    windowId: number,
+
+    setWindowProps: (wp: WindowProps) => void,
+    addWindow: () => void,
+    removeWindow: () => void,
+
     activeItemIndex: number,
     items: Array<Item>,
 
@@ -21,11 +30,15 @@ type ContextValue = {
 };
 
 const defaultContext: ContextValue = {
+    windowId: 1,
     title: "Queeg",
-    icon: "icon.ico",
-    nav: { position: "left" },
+    icon: "icon.png",
+    nav: { position: "left", size: 150 },
     activeItemIndex: 0,
     items: [],
+    setWindowProps: () => {},
+    addWindow: () => {},
+    removeWindow: () => {},
     setActiveItemIndex: () => {},
     addItem: async () => 0,
     saveItem: () => {},
@@ -36,17 +49,46 @@ const defaultContext: ContextValue = {
 
 const Context = React.createContext(defaultContext);
 
-export function Provider({ children, index }) {
+export function Provider({ children, windowId }: { children: React.ReactChild ,windowId: number }) {
 
+    const [ title, setTitle ] = React.useState(defaultContext.title);
+    const [ icon, setIcon ] = React.useState(defaultContext.icon);
     const [ items, setItems ] = React.useState<Item[]>(defaultContext.items);
     const [ activeItemIndex, setActiveItemIndex ] = React.useState(defaultContext.activeItemIndex);
 
     React.useEffect(() => {
-        getItemsForContext().then(items => setItems(items));
+        getItemsForContext(windowId).then(items => setItems(items));
+        getWindows().then(ws => ws.filter(({ id }) => id === windowId).forEach(({ title, icon }) => {
+            setTitle(title);
+            setIcon(icon);
+        }));
     }, []);
 
+    React.useEffect(() => {
+        document.title = title;
+    }, [ title ]);
+
     return <Context.Provider value={{
-        ...defaultContext,
+        windowId,
+        title,
+        icon,
+        nav: defaultContext.nav,
+
+        addWindow: () => {
+            addWindow({
+                id: 0,
+                icon: "icon.png",
+                title: "Queeg | New window"
+            });
+        },
+        removeWindow: () => {
+            deleteWindow(windowId);
+        },
+        setWindowProps: ({ title, icon }: WindowProps) => {
+            setTitle(title);
+            setWindow({ id: windowId, title, icon });
+        },
+
         items,
         activeItemIndex,
         setActiveItemIndex: activeItemIndex => {
@@ -60,19 +102,24 @@ export function Provider({ children, index }) {
             }));
         },
         addItem: async item => {
-            const id = await addItem(item);
-            const items = await getItemsForContext();
-            setItems(items);
+            const id = await addItem({ ...item, windowId });
+            const items = await getItemsForContext(windowId);
+            setItems(items.map(item => {
+                if (item.static.id === id) {
+                    return { ...item, dynamic: { ...item.dynamic, showForm: true } };
+                }
+                return item;
+            }));
             setActiveItemIndex(items.length - 1);
             return id;
         },
         saveItem: async item => {
             await setItem(item);
-            setItems(await getItemsForContext());
+            setItems(await getItemsForContext(windowId));
         },
         deleteItem: async id => {
             await deleteItem(id);
-            setItems(await getItemsForContext());
+            setItems(await getItemsForContext(windowId));
             setActiveItemIndex(0);
         },
         setDynamicItemProps: (index, set) => {
@@ -97,20 +144,21 @@ export function Provider({ children, index }) {
                 }
                 await setItem({ ...item.static, order });
             }
-            setItems(await getItemsForContext());
+            setItems(await getItemsForContext(windowId));
         },
     }}>
         {children}
     </Context.Provider>
 }
 
-async function getItemsForContext(): Promise<Item[]> {
-    const staticProps = await getItems();
+async function getItemsForContext(windowId: number): Promise<Item[]> {
+    const staticProps = await getItems(windowId);
     
     return staticProps.sort((a, b) => a.order - b.order).map(staticProp => {
         return {
             static: staticProp,
             dynamic: {
+                showForm: false,
                 title: staticProp.name,
                 alert: null,
                 reload: false,
