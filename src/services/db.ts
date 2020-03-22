04ba84680e565489fd8b795377cdc7e19e5f8978
Filip Paulů <ing.fenix@seznam.cz>
2020-03-22 21:19:14 +0100

    Add window endpoints for db

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,35 +1,55 @@
 import { insert, update, select, remove } from "easy-db-node";
 
-import { StaticItemProps } from "../types";
+import { StaticItemProps, WindowSettings } from "../types";
 
-type WindowsConfiguration = {
-    position: "top" | "left",
-};
+const ITEM_COLLECTION = "item";
+const WINDOW_COLLECTION = "window";
 
-export async function getWindowsConfiguration(): Promise<WindowsConfiguration> {
-    const conf = await select("window", 1);
+// Windows
+export async function getWindows(): Promise<WindowSettings[]> {
+    const settings = await select(WINDOW_COLLECTION);
 
-    if (conf) {
-        return { position: "left" };
+    if (Object.keys(settings).length === 0) {
+        // first run
+        await addWindow({ id: 1, icon: "icon.png", title: "Queeg" });
+        return await getWindows();
     } else {
-        return conf;
+        return Object.keys(settings).map(id => ({ ...settings[id], id }));
     }
 }
 
+export async function addWindow(window: WindowSettings): Promise<number> {
+    return await insert(WINDOW_COLLECTION, window);
+}
+export async function setWindow(window: WindowSettings): Promise<void> {
+    await update(WINDOW_COLLECTION, window.id, window);
+}
+export async function deleteWindow(id: number): Promise<void> {
+    await remove(WINDOW_COLLECTION, id);
+}
 
-export async function getItems(): Promise<StaticItemProps[]> {
-    const items = await select("item");
-    return Object.keys(items).map(id => ({ ...items[id], id }));
+// Items
+export async function getItems(windowId: number): Promise<StaticItemProps[]> {
+    console.log({windowId})
+    const items = await select(ITEM_COLLECTION);
+    return Object.keys(items).map(id => ({ ...items[id], id })).filter(item => {
+        // back compatibility
+        if (!item.windowId) {
+            item.windowId = 1;
+        }
+
+        return item.windowId == windowId;
+    });
 }
 
 export async function addItem(item: StaticItemProps): Promise<number> {
-    return await insert("item", item);
+    return await insert(ITEM_COLLECTION, item);
 }
 
 export async function setItem(item: StaticItemProps): Promise<void> {
-    await update("item", item.id, item);
+    await update(ITEM_COLLECTION, item.id, item);
 }
 
 export async function deleteItem(id: number): Promise<void> {
-    await remove("item", id);
+    await remove(ITEM_COLLECTION, id);
 }
