508232492a6ac509e6027196c6889f5588bf1fea
Filip Paulů <ing.fenix@seznam.cz>
2020-03-22 21:20:23 +0100

    Create form for window

---
+++ src/components/FormWindow.tsx
@@ -0,0 +1,148 @@
+import { readdir } from "fs";
+import { resolve } from "path";
+
+import * as React from "react";
+
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import Button from '@material-ui/core/Button';
+
+import IconButton from '@material-ui/core/IconButton';
+import SaveIcon from '@material-ui/icons/Save';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloseIcon from '@material-ui/icons/Close';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+
+import Alert from '@material-ui/lab/Alert';
+
+import useContext from "./Context";
+
+const iconPath = resolve(__filename, "..", "..", "..", "img", "icons");
+
+export default function WindowForm() {
+
+    const { title, icon, nav, setWindowProps, addWindow, removeWindow } = useContext();
+
+    const [icons, setIcons] = React.useState<string[] | null>(null);
+    const [editProps, setEditProps] = React.useState({ title, icon, nav });
+    const [showSnackbar, setShowSnackbar] = React.useState<null | string>(null);
+
+    React.useEffect(() => {
+        readdir(iconPath, { encoding: "utf8" }, (err, files) => {
+            if (files) {
+                setIcons(files.filter(name => name.endsWith(".png")));
+            }
+        });
+    }, []);
+
+
+    return <form>
+        <h1>Window settings</h1>
+
+        <Alert severity="warning">You need to restart the application to take effect.</Alert>
+
+        <TextField
+            required
+            fullWidth
+            id="title"
+            label="Window title"
+            margin="normal"
+            value={editProps.title}
+            onChange={e => setEditProps({ ...editProps, title: e.target.value })}
+        />
+        {icons === null && <div>Loading icons...</div>}
+        {icons !== null && <TextField
+            select
+            fullWidth
+            id="icon"
+            label="Icon"
+            margin="normal"
+            value={editProps.icon}
+            onChange={e => setEditProps({ ...editProps, icon: e.target.value })}
+        >
+            {icons.map(icon => (
+                <MenuItem key={icon} value={icon}>
+                    <img style={{ height: "20px", marginRight: "16px" }} src={resolve(iconPath, icon)} /> {" "}
+                    {icon}
+                </MenuItem>
+            ))}
+        </TextField>}
+
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button
+                variant="contained"
+                color="primary"
+                startIcon={<SaveIcon />}
+                onClick={() => {
+                    setWindowProps(editProps);
+                    setShowSnackbar("Changes saved, restart application to take effect!");
+                }}>
+                Save changes
+            </Button>
+            <Button
+                variant="contained"
+                color="default"
+                startIcon={<AddIcon />}
+                onClick={() => {
+                    addWindow();
+                    setShowSnackbar("Window added, restart application to take effect!");
+                }}>
+                Add window
+            </Button>
+            <Button
+                variant="contained"
+                color="secondary"
+                startIcon={<DeleteIcon />}
+                onClick={() => {
+                    removeWindow();
+                    setShowSnackbar("Window deleted, restart application to take effect!");
+                }}>
+                Delete window
+            </Button>
+        </div>
+
+        <Snackbar
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            open={showSnackbar !== null}
+            autoHideDuration={5000}
+            onClose={() => setShowSnackbar(null)}
+        >
+            <MySnackbarContentWrapper
+                onClose={() => setShowSnackbar(null)}
+                message={showSnackbar}
+            />
+        </Snackbar>
+    </form>;
+}
+
+export interface Props {
+    className?: string;
+    message?: string;
+    onClose?: () => void;
+}
+
+function MySnackbarContentWrapper(props: Props) {
+    const { className, message, onClose, ...other } = props;
+
+    return (
+        <SnackbarContent
+            aria-describedby="client-snackbar"
+            message={<div style={{ display: "flex", alignItems: "center" }}>
+                <CheckCircleIcon />
+                {message}
+            </div>}
+            action={[
+                <IconButton key="close" aria-label="close" color="inherit" onClick={onClose}>
+                    <CloseIcon />
+                </IconButton>,
+            ]}
+            {...other}
+        />
+    );
+}
